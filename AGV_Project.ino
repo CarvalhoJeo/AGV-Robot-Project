1582bd9147008f57f93a549fdaaad62dc9415555
Carvalho <75193903+CarvalhoJeo@users.noreply.github.com>
2026-05-12 17:49:06 -0300

    fix: line follower sem kP

--- AGV_Project.ino
+++ AGV_Project.ino
@@ -1,19 +1,20 @@
-// Motor A (left)
 #define motorA_pwm 11
-#define motorA_fwd 12
-#define motorA_bcw 13
+#define motorA_fwd 13
+#define motorA_bcw 12
 
-// Motor B (right)
 #define motorB_pwm 5
-#define motorB_fwd 6
-#define motorB_bcw 7
+#define motorB_fwd 7
+#define motorB_bcw 6
 
 #define left_IR  A0
 #define right_IR A1
 
 #define line_counter_IR A3
+#define front_IR        2    // porta sensor de anti-colisão
 
-#define KP         0.5f
+#define BLACK_THRESHOLD 200
+#define BASE_SPEED      120
+#define CORRECTION      50 //velocidade da curva
 
 enum State {
   FOLLOW_LINE,
@@ -24,7 +25,18 @@ enum State {
 
 enum State actual_state = FOLLOW_LINE;
 
+volatile bool obstacle_flag = false;
+
+void onObstacle() {
+  if (digitalRead(front_IR) == LOW) {
+    obstacle_flag = true;
+  } else {
+    obstacle_flag = false;
+  }
+}
+
 void setup() {
+  Serial.begin(9600);
   pinMode(motorA_pwm, OUTPUT);
   pinMode(motorA_fwd, OUTPUT);
   pinMode(motorA_bcw, OUTPUT);
@@ -32,16 +44,26 @@ void setup() {
   pinMode(motorB_pwm, OUTPUT);
   pinMode(motorB_fwd, OUTPUT);
   pinMode(motorB_bcw, OUTPUT);
+
+  pinMode(front_IR, INPUT);
+  attachInterrupt(digitalPinToInterrupt(front_IR), onObstacle, CHANGE);
 }
 
 void loop() {
   switch (actual_state) {
     case FOLLOW_LINE:
-      followLine();
+      if (obstacle_flag) {
+        stopMotors();
+        actual_state = STOP;
+      } else {
+        followLine();
+      }
       break;
 
     case STOP:
-      stopMotors();
+      if (!obstacle_flag) {
+        actual_state = FOLLOW_LINE;
+      }
       break;
 
     case TURN:
@@ -50,7 +72,8 @@ void loop() {
     case TEST:
       driveForward(200);
       delay(2000);
-      driveBackward(200);
+      stopMotors();
+      delay(1000);
       break;
   }
 }
@@ -95,15 +118,13 @@ void stopMotors() {
 }
 
 void followLine() {
-  int leftVal  = analogRead(left_IR);
-  int rightVal = analogRead(right_IR);
+  bool leftOnLine  = analogRead(left_IR)  > BLACK_THRESHOLD;
+  bool rightOnLine = analogRead(right_IR) > BLACK_THRESHOLD;
 
-  int error      = leftVal - rightVal;
-  int correction = (int)(KP * error);
+  int correction = 0;
+  if      (leftOnLine  && !rightOnLine) correction =  CORRECTION;
+  else if (rightOnLine && !leftOnLine)  correction = -CORRECTION;
 
-  int speedA = constrain(150 + correction, -255, 255);
-  int speedB = constrain(150 - correction, -255, 255);
-
-  setMotorA(speedA);
-  setMotorB(speedB);
+  setMotorA(constrain(BASE_SPEED - correction, -255, 255));
+  setMotorB(constrain(BASE_SPEED + correction, -255, 255));
 }
